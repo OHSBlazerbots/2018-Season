bed1e6e21936eacfe11da558c6bf0184f28de071
Overland Blazerbots <ohsblazerbots@gmail.com>
2018-02-01 18:58:27 -0700

    Started adding in vision processing code.

--- src/org/usfirst/frc/team3807/robot/Robot.java
+++ src/org/usfirst/frc/team3807/robot/Robot.java
@@ -3,10 +3,15 @@ package org.usfirst.frc.team3807.robot;
 import org.usfirst.frc.team3807.robot.commands.CommandBase;
 import org.usfirst.frc.team3807.robot.commands.autonomous.DoNothingAuto;
 import org.usfirst.frc.team3807.robot.commands.autonomous.DriveForward;
+import org.usfirst.frc.team3807.robot.controllers.GripPipeline;
 import org.usfirst.frc.team3807.robot.controllers.TalonSpeedController;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -47,6 +52,7 @@ public class Robot extends IterativeRobot{
 			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture("cam0",0);
             camera.setResolution(320, 240);
             camera.setFPS(11);
+            GripPipeline.process(Camera);
         }).start();
 		
 	}
